032894b63728f2b0bc72620ef0a0041ea82764f0
daniellemanos <59614307+daniellemanos@users.noreply.github.com>
2020-02-04 14:10:18 -0500

    updates

--- Problem 2/Problem2b.m
+++ Problem 2/Problem2b.m
@@ -1,6 +1,8 @@
 % input number and unit
 str = input('Input time in seconds (s), minutes (m), hours (h), or days (d)' , 's');
 
+% Not sure how to do this since the input is now a string instead of just a
+% number
 % reject non-numeric values
 %if ~isnumeric(str)
   % error("input must be a number with unit")
@@ -13,16 +15,23 @@ numb = newStr{1};
 % convert the string to a number
 time = str2num(numb);
 
-% differentiate between units
-
+% extract the unit
+unit = newStr{2};
+% differentiate between units and convert to seconds
+if unit == 's'
+    T = time
+elseif unit == 'm'
+    T = time*60;
+elseif unit == 'h'
+    T = time*3600;
+elseif unit == 'd'
+    T = time*86400;
+end
 
 % define constants: earth's radius, G, earth's mass
 R = 6.371E6;
 G = 6.67E-11;
 M = 5.98E24;
-
-% convert to seconds
-%T = T*60;
 
 % calculate height in meters
 h = (G*M*T^2/(4*pi^2))^(1/3)-R; 
@@ -40,5 +49,11 @@ v = ((G * M)/ (h + R))^(1/2);
 v = v/1000;
 
 % round answers
-xr = round(x)
-vr = round(v)
+xr = round(x);
+vr = round(v);
+
+% display answers
+disp('The height in km is')
+disp(xr)
+disp('The velocity in km/s is')
+disp(vr)
