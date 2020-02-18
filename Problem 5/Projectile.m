bc1d620feacf527298fd367a777695df844eeb6c
daniellemanos <59614307+daniellemanos@users.noreply.github.com>
2020-02-18 13:26:54 -0500

    updates

--- Problem 5/Projectile.m
+++ Problem 5/Projectile.m
@@ -3,7 +3,7 @@ function [x,t] = Projectile(vi,viunit,a)
 % range and time of flight.
 
 % define function contraints
-if a <=0 | a >= 0
+if a <=0 || a >= 90
     error("angle must be between 0 and 90 degrees")
 end
 
@@ -16,21 +16,32 @@ if ~isnumeric(a)
 end
 
 % convert units to m/s
-if viunit == mph
+if viunit == 'mph'
     vinew = (vi/2.237);
-elseif viunit == m/s
+elseif viunit == 'm/s'
     vinew = vi;
 end
 
 % find components of initial velocity
-vix = vinew*cos(a);
-viy = vinew*sin(a);
+vix = vinew*cosd(a);
+viy = vinew*sind(a);
 
 % define gravity constant
 g = 9.81;
 
+% calculate time of flight
+t = (2*viy)/g;
+
 % calculate range of flight
+x = (2*vix*viy)/g;
 
+% graph trajectory of projectile
+figure()
+plot(linspace(0,t,10),linspace(0,x,10),'-ok')
+xlabel('Time of Flight (s)')
+ylabel('Range of Flight (m)')
+title('Range vs. Time of a Projectile')
+%an = animatedline(linspace(0,t,10),linspace(0,x,10))
+% not sure how to animate the line - I've tried a few different things and
+% none of them work
 end
-
-
---
+++ Problem 6/motion.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Starting point for Problem 6a
+"""
+    
+
+def projectile(v,ang,h,g):
+    
+    return r,tf,hmax    
+    
+
+def freefall(h,v,g):
+    
+    return vf,tf
+    
+    
+
+    
