0fdf056e054b42e3f9ac98babd8b602687afb7fa
daniellemanos <59614307+daniellemanos@users.noreply.github.com>
2020-02-18 00:37:55 -0500

    Update Projectile.m

--- Problem 5/Projectile.m
+++ Problem 5/Projectile.m
@@ -1,3 +1,36 @@
-function [y,t] = Projectile(vi,a)
+function [x,t] = Projectile(vi,viunit,a)
+% Uses initial velocity and launch angle of a projectile to calculate the
+% range and time of flight.
 
-% input
+% define function contraints
+if a <=0 | a >= 0
+    error("angle must be between 0 and 90 degrees")
+end
+
+if ~isnumeric(vi)
+    error("velocity must be a number")
+end
+
+if ~isnumeric(a)
+    error("angle must be a number")
+end
+
+% convert units to m/s
+if viunit == mph
+    vinew = (vi/2.237);
+elseif viunit == m/s
+    vinew = vi;
+end
+
+% find components of initial velocity
+vix = vinew*cos(a);
+viy = vinew*sin(a);
+
+% define gravity constant
+g = 9.81;
+
+% calculate range of flight
+
+end
+
+
