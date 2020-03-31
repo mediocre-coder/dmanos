b511e335b78d2a70c125af700c9e3205735f2e1a
daniellemanos <59614307+daniellemanos@users.noreply.github.com>
2020-03-30 22:52:39 -0400

    updates 3/30

---
+++ Problem 7/Problem7a.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 28 20:41:23 2020
+
+@author: danie
+"""
+
+# import pandas
+import pandas as p
+
+# import data
+p.read_csv('climate.txt')
+p.reaf_csv('GlobalTempbyMonth.txt')
+
+# determine the hottest 10 years
