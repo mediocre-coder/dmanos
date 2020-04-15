b27c249e2d792e5b908f3342431129947e8b5455
daniellemanos <59614307+daniellemanos@users.noreply.github.com>
2020-04-15 16:10:54 -0400

    updates 4/15

--- Final/planetarypositions.py
+++ Final/planetarypositions.py
@@ -8,7 +8,6 @@ Created on Tue Mar 31 20:16:34 2020
 # import packages
 import ephem as e # this package is useful in astronomical calculations
 import matplotlib.pyplot as mp
-import numpy as np
 
 # create an object for each planet
 me = e.Mercury();
@@ -21,28 +20,26 @@ n = e.Neptune();
 p = e.Pluto();
 
 # write a function that takes inputs of planet and date (as a string of 
-# format 'yyyy/mm/dd') and returns a graph of the planet's position relative to
-# the Earth, which is located at the origin
+# format 'yyyy/mm/dd') and returns a graph of the planet's position relative 
+# to the Earth, which is located at the origin
 def position(pl,date):
     # position is returned in terms of right ascension (hours of arc around
     # celestial equator) and declination (degrees north of celestial equator)
-    pla = pl.compute(date);
+    pl.compute(date);
     
-    # return the value of right ascension (declination can be ignored because
-    # it is not necessary for 2D plotting) and convert to radians
-    #radra = repr(pl.ra);
-    #rias = pl.g_ra
-    deg = e.hours(pl.g_ra)
-    # normalize the angle so that is in the interval [0,2pi]
-    #radranorm = radra.norm;
+    # return the value of right ascension in degrees (declination can be 
+    # ignored because it is not necessary for 2D plotting) and convert to
+    # radians
+    deg = e.hours(pl.g_ra);
+    rad = repr(deg);
     
     # return the value of distance to Earth in km
     dist = pl.earth_distance;
     
     # convert angle and distance to polar coordinates
-    #r = dist;
-    #theta = radranorm;
+    r = dist;
+    theta = rad;
     
     # graph result
-    #graph = mp.polar(theta,r);
-    return pla, deg, dist#graph
+    graph = mp.polar(theta,r);
+    return graph
